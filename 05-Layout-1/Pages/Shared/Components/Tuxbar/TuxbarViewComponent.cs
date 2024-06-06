0002a7baa84ff1b6f95d9404cc99327f349594e4
Jonathan Danylko <Jonathan.Danylko@insight.com>
2024-06-06 13:46:02 -0400

    Initial commit

---
+++ 05-Layout-1/Pages/Shared/Components/Tuxbar/TuxbarViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tuxboard.Core.Domain.Entities;
+
+namespace Layout_1.Pages.Shared.Components.Tuxbar;
+
+[ViewComponent(Name = "tuxbar")]
+public class TuxbarViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke(Dashboard model)
+    {
+        return View(model);
+    }
+}
