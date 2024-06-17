b191399d4ee63548ed6c019d5a36cbd4fa0d0629
Jonathan Danylko <Jonathan.Danylko@insight.com>
2024-06-17 13:38:27 -0400

    - Move Show/Hide methods to BaseDialog - Clean up Layout Projects

--- 06-Layout-2/wwwroot/src/tuxboard/dialog/BaseDialog.ts
+++ 06-Layout-2/wwwroot/src/tuxboard/dialog/BaseDialog.ts
@@ -1,33 +1,15 @@
 ﻿import * as bootstrap from "bootstrap";
-import { hideElement, showElement } from "../common";
 
 export class BaseDialog {
 
     protected dialogBodySelector = ".modal-body";
-    protected generalOverlaySelector = ".overlay";
-    protected loadingSelector = ".loading-status";
-    protected dialogOverlaySelector = this.generalOverlaySelector + this.loadingSelector;
 
     constructor(protected selector: string) { }
 
-    // private getBackdrops = () => Array.from(document.querySelectorAll(".modal-backdrop"));
-
     public getDialogInstance = () => bootstrap.Modal.getOrCreateInstance(this.getDialog());
+    public getDom = () => this.getDialog();
     public getDialog = () => document.querySelector(this.selector);
-    public getDom = () => document.querySelector(this.selector);
-    public getOverlay = () => this.getDom().querySelector(this.generalOverlaySelector);
 
-    public showOverlay = () => {
-        const overlay = this.getOverlay();
-        if (overlay && overlay.hasAttribute("hidden")) {
-            showElement(overlay);
-        }
-    }
-
-    public hideOverlay = () => {
-        const overlay = this.getOverlay();
-        if (overlay && !overlay.hasAttribute("hidden")) {
-            hideElement(overlay);
-        }
-    }
+    showDialog = () => this.getDialogInstance().show();
+    hideDialog = () => this.getDialogInstance().hide();
 }
