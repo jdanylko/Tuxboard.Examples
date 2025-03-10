d9e423989b67ecd8a8c261387ad91b1929e63b53
Jonathan Danylko <jdanylko@gmail.com>
2025-03-09 20:29:32 -0400

    Update titles

---
+++ 12-Creating-Widgets/Pages/Shared/Components/WidgetDialog/WidgetDialogViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tuxboard.Core.Domain.Dto;
+
+namespace CreatingWidgets.Pages.Shared.Components.WidgetDialog;
+
+[ViewComponent(Name="WidgetDialog")]
+public class WidgetDialogViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke(List<WidgetDto> widgets)
+    {
+        return View(widgets);
+    }
+}
